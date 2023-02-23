0e54321c6aa487c53a5913053c582c23a2fed4ea
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-02-23 03:13:27 -0500

    [Ruchita] Add: Single player start position at 0

---
+++ Computation_SL/Computation_SL/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Computation_SL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { // snake & ladder game played with single player at start position 0
+    
+            int player_Position=0;
+         
+           Console.WriteLine("Welcome to Snake & Ladder game");
+            Console.WriteLine("start position of Player one is :" + player_Position);
+            
+            Console.ReadLine();
+        }
+    }
+}
