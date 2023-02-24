bccba5b247a3287126d8facaf36458df7c7a05f5
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-02-24 03:44:58 -0500

    [Ruchita] Add: Checks for option snake or ladder

--- Computation_SL/Computation_SL/Program.cs
+++ Computation_SL/Computation_SL/Program.cs
@@ -3,12 +3,43 @@
     internal class Program
     {
         static void Main(string[] args)
-        { //The player rolls the die to get the number between 1 to 6
+        {
+            //The player then checks for the option,They are no play,Snake or Ladder.
 
-            int Dice_rolled;
-         Random random= new Random();
-            Dice_rolled=random.Next(1,7);
-            Console.WriteLine("Number get after Dice rolled:" + Dice_rolled);
+            const int Snake = 0;
+            const int Ladder = 1;
+            const int NoGame = 2;
+            int position = 0;
+            Random random = new Random();
+            int diceRoll = random.Next(1, 7);
+            Console.WriteLine("Rolled Dice number is:" + diceRoll);
+            int choice = random.Next(0, 3);
+
+            switch (choice)
+            {
+                case Snake:
+                    position -= diceRoll;
+                    Console.WriteLine("Its a snake");
+                    if (position < 0)
+                    {
+                        Console.WriteLine("Player 1 position : 0");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Player 1 position : " + position);
+                    }
+                    break;
+                case Ladder:
+                    position += diceRoll;
+                    Console.WriteLine("Its a ladder");
+                    Console.WriteLine("Player 1 position : " + position);
+                    break;
+                case NoGame:
+                    Console.WriteLine("Its a NoGame");
+                    Console.WriteLine("Player 1 position : " + position);
+                    break;
+
+            }
             Console.ReadLine();
         }
     }
